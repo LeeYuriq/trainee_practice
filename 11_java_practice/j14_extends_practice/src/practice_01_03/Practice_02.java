db6a13d2421ea93f40cf21a3a1f80b6328d0d21a
LeeYuriq <0511youli@gmail.com>
2024-05-23 16:32:35 +0900

    practice0523

---
+++ 11_java_practice/j14_extends_practice/src/practice_01_03/Practice_02.java
@@ -0,0 +1,21 @@
+package practice_01_03;
+
+
+public class Practice_02 {
+
+	public static void main(String[] args) {
+
+		int[] lotto = new int[6];
+		
+		// int number = (int) (Math.random() * 45 + 1); // 참고
+		// 작성
+
+		
+		
+		// 결과 출력 코드 수정 x
+		System.out.println("[ 결과 ]");
+		for (int i : lotto) {
+			System.out.print("[" +i+ "]");
+		}
+	}
+}
---
+++ 11_java_practice/j14_extends_practice/src/practice_01_03/Practice_03.java
@@ -0,0 +1,9 @@
+package practice_01_03;
+
+
+public class Practice_03 {
+
+	public static void main(String[] args) {
+		// code 작성
+	}
+}
